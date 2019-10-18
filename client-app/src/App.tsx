04952ee9e7f55b7eaa36711dccb6d1b8aac2c0ef
khonoridez <oznyakis@gmail.com>
2019-10-18 10:26:45 -0400

    Installed semantic-ui-react, sample usage

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Header, Icon, List } from "semantic-ui-react";
 import axios from "axios";
 import "./App.css";
 
@@ -10,7 +10,6 @@ class App extends Component {
 
   componentDidMount() {
     axios.get("http://localhost:5000/api/values").then(response => {
-      console.log(response);
       this.setState({
         values: response.data
       });
@@ -19,15 +18,19 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-            {this.state.values.map((value: any) => (
-              <li key={value.id}>{value.name}</li>
-            ))}
-          </ul>
-        </header>
+      <div>
+        <Header as="h2">
+          <Icon name="folder open outline" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>
+              <List.Icon name="caret square right outline" />
+              <List.Content>{value.name}</List.Content>
+            </List.Item>
+          ))}
+        </List>
       </div>
     );
   }
