53460e5bb26c12491c80b41b8c195bf08deba1dd
khonoridez <oznyakis@gmail.com>
2019-10-20 14:05:28 -0400

    Refactor to use React Hooks

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,45 +1,35 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Icon, List } from "semantic-ui-react";
 import axios from "axios";
 import { IActivity } from "../models/activity";
 
-interface IState {
-  activities: IActivity[];
-}
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    activities: []
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get<IActivity[]>("http://localhost:5000/api/activities")
       .then(response => {
-        this.setState({
-          activities: response.data
-        });
+        setActivities(response.data);
       });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Header as="h2">
-          <Icon name="folder open outline" />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity: any) => (
-            <List.Item key={activity.id}>
-              <List.Icon name="caret square right outline" />
-              <List.Content>{activity.title}</List.Content>
-            </List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header as="h2">
+        <Icon name="folder open outline" />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>
+            <List.Icon name="caret square right outline" />
+            <List.Content>{activity.title}</List.Content>
+          </List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default App;
