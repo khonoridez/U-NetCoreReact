245272b801962dfedb1e0b4b4889aad84e8057b6
khonoridez <oznyakis@gmail.com>
2019-10-21 11:44:19 -0400

    implement ActivityForm component

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import { Grid, GridColumn } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 interface IProps {
   activities: IActivity[];
@@ -16,6 +17,7 @@ const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
       </GridColumn>
       <GridColumn width="6">
         <ActivityDetails />
+        <ActivityForm />
       </GridColumn>
     </Grid>
   );
