1baff6f3a7a97e78bb2d7e82a7636c444a978a00
khonoridez <oznyakis@gmail.com>
2019-10-23 14:56:17 -0400

    Separate Home Page, implement scroll to top

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,9 +5,11 @@ import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
+import ScrollToTop from "./app/layout/ScrollToTop";
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
