23ae59ab93b87977c956b90b2ee9e4cd479808f7
khonoridez <oznyakis@gmail.com>
2019-10-29 23:40:53 -0400

    Implement Photo upload buttons and toggles

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,6 +1,6 @@
 import { RootStore } from "./rootStore";
 import { IProfile } from "../models/profile";
-import { observable, action, runInAction } from "mobx";
+import { observable, action, runInAction, computed } from "mobx";
 import agent from "../api/agent";
 
 export default class ProfileStore {
@@ -12,6 +12,14 @@ export default class ProfileStore {
 
   @observable profile: IProfile | null = null;
   @observable loadingProfile = true;
+
+  @computed get isCurrentUser() {
+    if (this.rootStore.userStore.user && this.profile) {
+      return this.rootStore.userStore.user.username === this.profile.username;
+    } else {
+      return false;
+    }
+  }
 
   @action loadProfile = async (username: string) => {
     this.loadingProfile = true;
