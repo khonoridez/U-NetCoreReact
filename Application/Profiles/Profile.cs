8751e1a4338fc25218892c45ef813c31b56153fb
khonoridez <oznyakis@gmail.com>
2019-11-05 00:06:47 -0500

    Implement list of Followings, implement ProfileReader helper

--- Application/Profiles/Profile.cs
+++ Application/Profiles/Profile.cs
@@ -1,4 +1,5 @@
 ﻿using Domain;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace Application.Profiles
@@ -9,6 +10,11 @@ namespace Application.Profiles
         public string Username { get; set; }
         public string Image { get; set; }
         public string Bio { get; set; }
+
+        [JsonProperty("following")]
+        public bool IsFollowed { get; set; }
+        public int FollowersCount { get; set; }
+        public int FollowingCount { get; set; }
         public ICollection<Photo> Photos { get; set; }
     }
 }
